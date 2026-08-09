a4f8f2f3dbea00714fd425b556760390ad921bd3
Ricardo José de Souza <ti.ricardo.souza@gmail.com>
2026-08-08 23:53:08 -0300

    feat: adicionado o codigo do livro

--- livro.js
+++
@@ -1,9 +0,0 @@
-export class Livro {
-  constructor(titulo, autor, categoria, paginas, disponivel = true) {
-    this.titulo = titulo;
-    this.autor = autor;
-    this.categoria = categoria;
-    this.paginas = paginas;
-    this.disponivel = disponivel;
-  }
-}
