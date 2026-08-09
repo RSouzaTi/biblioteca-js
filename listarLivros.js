00f4947dc2935effcdb1ec4b4419719ada10d7b9
brenda-marinho <contatobrendamarinho@hotmail.com>
2026-08-09 11:39:35 -0300

    fix: correção dos imports e da listagem de livros

--- listarLivros.js
+++ listarLivros.js
@@ -1,14 +1,20 @@
-import {livros} from "./dadosLivros.js"; // importa o array de livros do arquivo dadosLivros.js
+import { dadosLivros } from "./dadosLivros.js";
 
-export function listarLivros() { // função que lista todos os livros cadastrados no sistema.
+export function exibirLivro(livro, indice) {
+    console.log(`\nLivro ${indice + 1}`);
+    console.log(`Título: ${livro.titulo.toUpperCase()}`);
+    console.log(`Autor: ${livro.autor}`);
+    console.log(`Categoria: ${livro.categoria}`);
+    console.log(`Número de páginas: ${livro.paginas}`);
+    console.log(
+        `Disponibilidade: ${livro.disponivel ? "Disponível" : "Emprestado"}`
+    );
+}
+
+export function listarLivros() {
     console.log("\n LISTA DE LIVROS \n");
 
-    livros.forEach((livro, indice) => { // percorre o array de livros e exibe as informações de cada livro no console.
-        console.log(`\nLivro ${indice + 1}`); // exibe o número do livro na lista (começando do 1)
-        console.log(`Título: ${livro.titulo.toUpperCase()}`); // exibe o título do livro em letras maiúsculas
-        console.log(`Autor: ${livro.autor}`); // exibe o autor do livro
-        console.log(`Categoria: ${livro.categoria}`); // exibe a categoria do livro
-        console.log(`Número de páginas: ${livro.paginas}`); // exibe o número de páginas do livro
-        console.log(`Disponibilidade: ${livro.disponivel ? "Disponível" : "Emprestado"}`); // exibe a disponibilidade do livro (disponível ou emprestado)
+    dadosLivros.forEach((livro, indice) => {
+        exibirLivro(livro, indice);
     });
 }
